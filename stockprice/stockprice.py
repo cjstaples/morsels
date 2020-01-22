6135c0d7fed5526bf544592d22861d4793922df5
Chuck Staples <cstaples@cashstar.com>
2020-01-22 17:29:11 -0500

    init pandas read w/data

--- stockprice/stockprice.py
+++ stockprice/stockprice.py
@@ -3,6 +3,8 @@
 #
 
 import sys
+import pandas as pd
+import numpy as np
 
 
 def main():
@@ -11,7 +13,10 @@ def main():
 
     #
     #
+    df = pd.read_csv('timeseries_samples_stock_count-20.csv')
 
+    # print(df)
+    # print(df.head(5))
 
     print()
     print('(stockprice) end::')
