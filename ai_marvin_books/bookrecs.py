8c057e1b8db40e3c9f4177a788979c92b7652f4b
Chuck Staples <cjstaples@gmail.com>
2025-02-04 15:46:29 -0500

    initial version, api key tweaking

--- ai_marvin_books/bookrecs.py
+++ ai_marvin_books/bookrecs.py
@@ -1,7 +1,7 @@
-# ///
-#
-#
-#
+# /// script
+# dependencies = [
+#   "marvin",
+# ]
 # ///
 
 #   Book Recs via AI
@@ -18,16 +18,6 @@ import marvin
 if "MARVIN_OPENAI_API_KEY" not in os.environ:
     sys.exit("Environment variable 'MARVIN_OPENAI_API_KEY' must be set before use")
 
-if __name__ == '__main__':
-
-    print("::: BOOK RECS :::")
-    print("::: Based on code by Bob Belderbos / PyBites :::")
-    print(":::")
-
-    print(":::")
-    print("::: END :::")
-    sys.exit()
-
 class Book(BaseModel):
     title: str = Field(..., description="Title of Book")
     author: str = Field(..., description="Author of Book")
@@ -39,3 +29,40 @@ def recommend_similar_books(query_book: Book) -> list[Book]:
     Given a Book object, return a list of similar books.
     Recommendations should consider similar genres.
     """
+
+if __name__ == '__main__':
+
+    print("::: BOOK RECS :::")
+    print("::: Based on code by Bob Belderbos / PyBites :::")
+    print(":::")
+
+    parser = argparse.ArgumentParser(
+        description="Recommend similar books using OpenAI"
+    )
+    parser.add_argument("--title", required=True, help="Book title")
+    parser.add_argument("--author", required=True, help="Book author")
+    args = parser.parse_args()
+
+    print(f"book title => {args.title}")
+    print(f"book author => {args.author}")
+
+    query_book = Book(
+        title=args.title,
+        author=args.author,
+        # year=int(args.year) if args.year else None,
+    )
+    recommendations = recommend_similar_books(query_book)
+    for book in recommendations:
+        print(f"{book.title} => {book.author} => {book.year}")
+
+    print("===")
+    print(f"initial: {query_book}")
+    print("===")
+
+
+    print(f"returns: {query_book}")
+    print("===")
+
+    print(":::")
+    print("::: END :::")
+    sys.exit()
