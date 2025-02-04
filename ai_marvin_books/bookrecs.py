7b6d01d994e17281d534fb05b60561bafb36c0cc
Chuck Staples <cjstaples@gmail.com>
2025-02-04 14:44:04 -0500

    class add

--- ai_marvin_books/bookrecs.py
+++ ai_marvin_books/bookrecs.py
@@ -28,3 +28,14 @@ if __name__ == '__main__':
     print("::: END :::")
     sys.exit()
 
+class Book(BaseModel):
+    title: str = Field(..., description="Title of Book")
+    author: str = Field(..., description="Author of Book")
+    year: int | None = Field(None, description="Year of Book Publication")
+
+@marvin.fn
+def recommend_similar_books(query_book: Book) -> list[Book]:
+    """
+    Given a Book object, return a list of similar books.
+    Recommendations should consider similar genres.
+    """
