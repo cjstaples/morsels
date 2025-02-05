e29c25407a2667f72b4a9e431c7878c88836b916
Chuck Staples <cjstaples@gmail.com>
2025-02-05 14:14:52 -0500

    uv meta sample / initial

---
+++ uv_meta_soup/uvmsample.py
@@ -0,0 +1,31 @@
+# ///
+# dependencies = [
+#   "bs4",
+#   "httpx",
+#   "typer"
+# ]
+# ///
+
+#   uv / metadata sample
+#   Based on code by Bob Belderbos / PyBites
+#
+
+import sys
+import textwrap
+
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+
+    print("::: UV METADATA SAMPLE :::")
+    print("::: Based on code by Bob Belderbos / PyBites :::")
+    print(":::")
+
+    ui_tweaking = True
+
+
+    print("===")
+
+    print(":::")
+    print("::: END :::")
+    sys.exit()
