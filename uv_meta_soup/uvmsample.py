1817bcac0464fd12fbf390dd1f3066d7d968b4e2
Chuck Staples <cjstaples@gmail.com>
2025-02-05 15:37:11 -0500

    incremental updates - start fleshing out app flow

--- uv_meta_soup/uvmsample.py
+++ uv_meta_soup/uvmsample.py
@@ -12,8 +12,35 @@
 
 import sys
 import textwrap
+from contextlib import nullcontext
 
 from bs4 import BeautifulSoup
+import httpx
+import typer
+
+app = typer.Typer()
+ui_tweaking = True
+
+BASE_URL = "https://www.googleapis.com/books/v1/volumes"
+BOOK_URL = BASE_URL + "/{}"
+SEARCH_URL = BASE_URL + "?q={}&langRestrict=en"
+
+def search_books(term: str):
+    """Search books by term."""
+    books = []
+    return books
+
+def get_book_details(book_id: str):
+    """Retrieve details for a specific book."""
+    resp = None
+    return resp
+
+def clean_and_shorten_description(description: str, max_length: int = 300):
+    """Remove HTML Tags from the description and truncate it."""
+    pass
+    return
+
+
 
 if __name__ == '__main__':
 
@@ -21,9 +48,8 @@ if __name__ == '__main__':
     print("::: Based on code by Bob Belderbos / PyBites :::")
     print(":::")
 
-    ui_tweaking = True
-
-
+    print("===")
+    # app()
     print("===")
 
     print(":::")
