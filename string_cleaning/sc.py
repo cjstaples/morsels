716b255cc7b2f922d42385b8ca3c0eb8817b7c9b
Chuck Staples <cjstaples@gmail.com>
2025-03-12 14:39:47 -0400

    string cleaning

--- string_cleaning/sc.py
+++ string_cleaning/sc.py
@@ -3,17 +3,31 @@
 #
 
 import sys
+import string
 
 # ---------------
 
 def main():
-    print('(template) main:')
+    print('(string cleaning) main:')
     print()
 
-    # todo: user input
-    #
+    input_string_1 = "#%^$#Something, to. learn- Python with&^$%$%#"
+    input_string_2 = "#%^$#Something, else** to. learn- Python more with&^$%$%#"
 
-    print('(template) end::')
+    # make a translation table, mapping away all punctuation characters to nothing
+    # works with other subsets of string.xxx e.g. string.ascii_letters or string.whitespace
+    # translation_table = str.maketrans('', '', string.ascii_letters)
+
+    translation_table_punc = str.maketrans('', '', string.punctuation)
+    translation_table_ascii = str.maketrans('', '', string.ascii_letters)
+
+    cleaned_str_1 = input_string_1.translate(translation_table_punc)
+    print(f"cleaned_str_1 = '{cleaned_str_1}'\n")
+
+    cleaned_str_2 = input_string_2.translate(translation_table_ascii)
+    print(f"cleaned_str_2 = '{cleaned_str_2}'\n")
+
+    print('(string cleaning) end::')
     return 0
 
 # ----------------------------------------
